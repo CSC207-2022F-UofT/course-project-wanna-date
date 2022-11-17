151780a3c1ad44c5e2d81d3d5989e376720f5d5a
supriyavasanth <supriya.vasanthakumaran@mail.utoronto.ca>
2022-11-17 13:58:16 -0500

    I did some GUI for the chat box, for now it has a send button, a text field and the message appears as a label when the send button is pressed. (Chatbox.java)

---
+++ src/main/java/UseCase/OpenChatbox.java
@@ -0,0 +1,24 @@
+package UseCase;
+
+import Entities.ChatBox;
+import UseCase.RetrieveLike;
+
+public class OpenChatbox {
+
+    public static void main(String[] args) {
+        ChatBox chatBox = new ChatBox();
+
+        // check if users has anyone mutually like
+//        if (UseCase.RetrieveLike == null)
+//        {
+//            show message that says, connect with more people to begin a chat
+//
+//        }
+//
+//        else
+//        {
+//             open chatbox
+        // ChatBox chatBox = new ChatBox();
+//        }
+    }
+}
