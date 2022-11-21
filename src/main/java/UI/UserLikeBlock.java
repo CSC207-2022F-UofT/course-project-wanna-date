0a0d26858823208e2162df4e02f2819c30bd3386
adelineleonardi <adeline.leonardi@mail.utoronto.ca>
2022-11-21 14:55:15 -0500

    - Create a function in the UI to be called by another UI - Update minor error in UI

--- src/main/java/UI/UserLikeBlock.java
+++ src/main/java/UI/UserLikeBlock.java
@@ -17,6 +17,9 @@ public class UserLikeBlock implements ActionListener {
     ControllerLikeBlock controller;
     //Initiate buttons, frame and label
     JFrame profile = new JFrame();
+    JLabel title = new JLabel("Aphrodite");
+    JLabel pageTitle = new JLabel("User Profile");
+
     JLabel fullName = new JLabel("Full Name: ");
     JLabel age = new JLabel();
     JLabel pronoun = new JLabel();
@@ -47,89 +50,100 @@ public class UserLikeBlock implements ActionListener {
 
         //Create frame
         profile.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //THE CLOSE BUTTON
-        profile.setSize(1000, 800); //size of the frame
+        profile.setSize(800, 800); //size of the frame
         profile.setLayout(null); //customize where u want everything to go
         profile.setVisible(true); //make the frame visible
 
+        title.setBounds(10, 10, 100, 100);
+        title.setFont(new Font(null, Font.BOLD, 16));
+        title.setVisible(true);
+
+        pageTitle.setBounds(300, 20, 100, 100);
+        pageTitle.setFont(new Font(null, Font.BOLD, 14));
+        title.setVisible(true);
+
         //create full name text and set text to include the user's full name
-        fullName.setBounds(10,10, 300, 100); //coordinate and size of the label
+        fullName.setBounds(20,50, 300, 100); //coordinate and size of the label
         fullName.setFont(new Font(null, Font.PLAIN, 14));
         fullName.setText("Full Name: " + user2.get_full_name());
+        fullName.setFocusable(false);
         fullName.setVisible(true);
 
         //create age text and set text to include the user's age
-        age.setBounds(10,70, 300, 100); //coordinate and size of the label
+        age.setBounds(20,120, 300, 100); //coordinate and size of the label
         age.setFont(new Font(null, Font.PLAIN, 14));
         age.setText("Age: " + user2.get_age());
         age.setVisible(true);
 
         //create pronoun text
-        pronoun.setBounds(10,140, 300, 100); //coordinate and size of the label
+        pronoun.setBounds(20,190, 300, 100); //coordinate and size of the label
         pronoun.setFont(new Font(null, Font.PLAIN, 14));
         pronoun.setText("Pronouns: " + user2.get_pronouns());
         pronoun.setVisible(true);
 
         //create country text
-        country.setBounds(10,210, 400, 100); //coordinate and size of the label
+        country.setBounds(20,260, 400, 100); //coordinate and size of the label
         country.setFont(new Font(null, Font.PLAIN, 14));
         country.setText("Country of Origin: " + user2.get_location().get("country"));
         country.setVisible(true);
 
         //create province text
-        province.setBounds(10,280, 300, 100); //coordinate and size of the label
+        province.setBounds(20,330, 300, 100); //coordinate and size of the label
         province.setFont(new Font(null, Font.PLAIN, 14));
         province.setText("Province: " + user2.get_location().get("province"));
         province.setVisible(true);
 
         //create city text
-        city.setBounds(10,350, 300, 100); //coordinate and size of the label
+        city.setBounds(20,400, 300, 100); //coordinate and size of the label
         city.setFont(new Font(null, Font.PLAIN, 14));
         city.setText("City: " + user2.get_location().get("city"));
         city.setVisible(true);
 
         //create gender text
-        gender.setBounds(10,420, 300, 100); //coordinate and size of the label
+        gender.setBounds(20,470, 300, 100); //coordinate and size of the label
         gender.setFont(new Font(null, Font.PLAIN, 14));
         gender.setText("Gender: " + user2.get_gender());
         gender.setVisible(true);
 
         //create sexuality text
-        sexuality.setBounds(10,490, 300, 100); //coordinate and size of the label
+        sexuality.setBounds(20,540, 300, 100); //coordinate and size of the label
         sexuality.setFont(new Font(null, Font.PLAIN, 14));
         sexuality.setText("Sexuality: " + user2.get_sexuality());
         sexuality.setVisible(true);
 
         //create interests text
-        interests.setBounds(10,560, 700, 100); //coordinate and size of the label
+        interests.setBounds(20,610, 700, 100); //coordinate and size of the label
         interests.setFont(new Font(null, Font.PLAIN, 14));
         interests.setText("Interest: " + user2.get_interests());
         interests.setVisible(true);
 
         //create like button
-        like.setBounds(10,660, 60, 40); //coordinate and size of the label
+        like.setBounds(100,700, 60, 40); //coordinate and size of the label
         like.setFocusable(false);
         like.addActionListener(this);
         like.setBackground(Color.red);
         like.setOpaque(true);
 
         //create block button
-        block.setBounds(700,660, 60, 40); //coordinate and size of the label
+        block.setBounds(500,700, 60, 40); //coordinate and size of the label
         block.setFocusable(false);
         block.addActionListener(this);
         block.setBackground(Color.black);
         block.setOpaque(true);
 
         //create string that will appear after we finish with the like backend work
-        likedString.setBounds(700,20, 100, 100); //coordinate and size of the label
-        likedString.setFont(new Font(null, Font.PLAIN, 14));
+        likedString.setBounds(600,20, 100, 100); //coordinate and size of the label
+        likedString.setFont(new Font(null, Font.ITALIC, 14));
         likedString.setVisible(false);
 
         //create string that will appear after we finish with the block backend work
-        blockedString.setBounds(700,50, 100, 100); //coordinate and size of the label
-        blockedString.setFont(new Font(null, Font.PLAIN, 14));
+        blockedString.setBounds(600,50, 100, 100); //coordinate and size of the label
+        blockedString.setFont(new Font(null, Font.ITALIC, 14));
         blockedString.setVisible(false);
 
         //Add everything to the frame
+        profile.add(title);
+        profile.add(pageTitle);
         profile.add(fullName);
         profile.add(age);
         profile.add(pronoun);
@@ -139,6 +153,7 @@ public class UserLikeBlock implements ActionListener {
         profile.add(gender);
         profile.add(sexuality);
         profile.add(interests);
+        profile.repaint();
 
         profile.add(block);
         profile.add(like);
@@ -163,6 +178,10 @@ public class UserLikeBlock implements ActionListener {
             blockedString.setText(textShow2);
             blockedString.setVisible(true);
         }
+    }
+
+    public void functionToCall(){
+        new UserLikeBlock(user1, user2, controller);
     }
 
     public static void main (String[] args) {
