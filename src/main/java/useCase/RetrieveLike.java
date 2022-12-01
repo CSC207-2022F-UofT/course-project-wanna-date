02322fcf5a355530b00a514a21edd5f387d3cc9d
adelineleonardi <adeline.leonardi@mail.utoronto.ca>
2022-12-01 14:09:58 -0500

    Create subclass for UserAccount, update the naming convention to call function in entities also edit few implementation in the ControllerLikeBlock

--- src/main/java/useCase/RetrieveLike.java
+++ src/main/java/useCase/RetrieveLike.java
@@ -4,6 +4,9 @@ import interfaceAdapters.PresenterLikeBlock;
 import java.util.ArrayList;
 
 public class RetrieveLike {
+    /** RetrieveLike class that will be connected to the presenter which is responsible to update view when
+     * action is given from the user.
+     */
 
     // Function to connect his class with ReceiveLike class
     public void connector() {
@@ -11,14 +14,15 @@ public class RetrieveLike {
         like_view(param1);
     }
 
+    // Function to call the presenter
     public void like_view(RetrieveLikeOutputBoundary view){
 
         view.presenterLike();
     }
-
+    //Function to determine mutualLike
     public boolean mutualLike(UserAccount user1, UserAccount user2){
-        ArrayList<UserAccount>users_liked_by_user1 = user1.get_liked_users();
-        ArrayList<UserAccount>users_liking_user1 = user1.get_liked_by_users();
+        ArrayList<UserAccount>users_liked_by_user1 = user1.getLikedUsers();
+        ArrayList<UserAccount>users_liking_user1 = user1.getLikedByUsers();
         return users_liking_user1.contains(user2) & users_liked_by_user1.contains(user2);
     }
 
