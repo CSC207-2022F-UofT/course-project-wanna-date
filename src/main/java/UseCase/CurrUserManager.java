f3bc4c1bdd513419878f18da8db210372412348d
lovinalokeswara <lovina.lokeswara@mail.utoronto.ca>
2022-12-03 19:23:40 -0500

    Added documentation for UserAccount and Database feature's classes

--- src/main/java/UseCase/CurrUserManager.java
+++ src/main/java/UseCase/CurrUserManager.java
@@ -2,24 +2,38 @@ package useCase;
 
 import entities.UserAccount;
 
+/** A useCase singleton Class that stores the current user's UserAccount object.
+ * */
 public class CurrUserManager {
 
-    private static CurrUserManager currUser;
+    // === Class variables ===
+
+    // The static variable representing this very own class.
+    private static CurrUserManager currUserManager;
+
+    // The variable representing the username of the current user.
     private String username;
 
-    private CurrUserManager() {
-    }
+    /** Private constructor function so this class cannot be instantiated outside this class.
+     * */
+    private CurrUserManager() {}
 
     public static CurrUserManager getCurrUserManager() {
-        if (currUser == null)
-            currUser = new CurrUserManager();
-        return currUser;
+        if (currUserManager == null)
+            currUserManager = new CurrUserManager();
+        return currUserManager;
     }
 
+    /** The function used to set the current user's username to this class's instance variable.
+     *
+     *  @param username  Represents the username of current user
+     * */
     public void setCurrUsername(String username) {
         this.username = username;
     }
 
+    /** The function used to get the current user's UserAccount.
+     * */
     public UserAccount getCurrUser() {
         DatabaseManager databaseManager = DatabaseManager.getDatabaseManager();
         return databaseManager.retrieveUserAccount(this.username);
--- src/main/java/UseCase/DataExportInterface.java
+++ src/main/java/UseCase/DataExportInterface.java
@@ -1,6 +1,8 @@
 package useCase;
 
+/** InputBoundary for DataExport.
+ *  Function with no implementation that will be overwritten in DatabaseManager class.
+ */
 public interface DataExportInterface {
-
     Object[][] writeDatabase();
 }
