ed5d540ced90297f1a0d9c180b0e7f28eee16817
supriyavasanth <supriya.vasanthakumaran@mail.utoronto.ca>
2022-12-04 17:23:41 -0500

    Trying to commit SendMessageController as it was not able to commit the first time I tried.

--- src/main/java/interfaceAdapters/SendMessageController.java
+++ src/main/java/interfaceAdapters/SendMessageController.java
@@ -1,20 +1,15 @@
-package interfaceAdapters;
+package InterfaceAdapters;
 
 import useCase.SendMessageIB;
 import useCase.SendMessageID;
 import useCase.ReceiveMessageOD;
 
-/**
- * A controller that calls on SendMessageIB to save the given message to the message database.
- */
-
 public class SendMessageController {
-    final SendMessageIB messageInput;
+    private final SendMessageIB messageInput;
 
     public SendMessageController(SendMessageIB messageInput) {
         this.messageInput = messageInput;
     }
 
-    public ReceiveMessageOD saveMessage(SendMessageID sendMessageID) {return messageInput.sendMessage(sendMessageID);}
+    ReceiveMessageOD saveMessage(SendMessageID sendMessageID) {return messageInput.sendMessage(sendMessageID);}
 }
-
