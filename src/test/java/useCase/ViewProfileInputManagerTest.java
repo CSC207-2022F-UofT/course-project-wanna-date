946589909fe2deace5da78e0f04e2bbcf23e9501
lovinalokeswara <lovina.lokeswara@mail.utoronto.ca>
2022-12-07 03:04:25 -0500

    Added documentation and fixed test classes for Login, Registration, and ViewProfile. Tested the tests inside these classes.

--- src/test/java/useCase/ViewProfileInputManagerTest.java
+++ src/test/java/useCase/ViewProfileInputManagerTest.java
@@ -1,7 +1,9 @@
 package useCase;
 
 import entities.UserAccount;
-import org.junit.Test;
+import frameworksDrivers.DataAccess;
+
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.Assertions;
 
 import java.util.ArrayList;
@@ -11,8 +13,63 @@ import java.util.ArrayList;
  */
 public class ViewProfileInputManagerTest {
 
-    // Create a new UserAccount for this test and save it to Database
-    public static void main(String[] args) {
+    /** Test cases searchInfo whether it returns the correct number of elements in the ArrayList.
+     * The ArrayList should return 11 variables of the user (all variables, excluding: likedUsers, likedByUsers,
+     * blockedUsers, blockedByUsers)
+     * */
+    @Test
+    public void searchInfoLength() {
+        // Create Database from database.csv
+        DataAccess dataAccess = new DataAccess();
+        dataAccess.readCSV();
+
+        // Show that username "lov123" exists in Database
+        DatabaseManager databaseManager = DatabaseManager.getDatabaseManager();
+        System.out.println(databaseManager.isUsernameExist("lov123"));
+
+        ViewProfileInputManager viewProfileInputManager = new ViewProfileInputManager();
+        ArrayList<String> info = viewProfileInputManager.searchInfo("lov123");
+
+        Assertions.assertEquals(11, info.size());
+    }
+
+//    /** Test cases searchInfo whether it returns String type of information contained in ArrayList.
+//     * It is important that all information in ArrayList are String type because this will affect the
+//     * code in the UI for ViewProfile.
+//     * */
+//    @Test
+//    public void searchInfoClassType() {
+//        // Create Database from database.csv
+//        DataAccess dataAccess = new DataAccess();
+//        dataAccess.readCSV();
+//
+//        // Show that username "lov123" exists in Database
+//        DatabaseManager databaseManager = DatabaseManager.getDatabaseManager();
+//        System.out.println(databaseManager.isUsernameExist("lov123"));
+//
+//        ViewProfileInputManager viewProfileInputManager = new ViewProfileInputManager();
+//        ArrayList<String> info = viewProfileInputManager.searchInfo("lov123");
+//
+//        boolean allString = true;
+//
+//        for (String variable: info) {
+//            if (!(variable instanceof String)) {
+//                allString = false;
+//            }
+//        }
+//
+//        Assertions.assertTrue(allString);
+//    }
+
+    /** Test cases searchInfo whether it returns the correct information of the newly added user.
+     * */
+    @Test
+    public void searchInfoCorrect() {
+        // Create Database from database.csv
+        DataAccess dataAccess = new DataAccess();
+        dataAccess.readCSV();
+
+        // Save a new user to database for the purpose of this testing
         UserAccount newUser = new UserAccount(
                 "lov456",
                 "Lovina",
@@ -28,21 +85,11 @@ public class ViewProfileInputManagerTest {
         );
         DatabaseManager databaseManager = DatabaseManager.getDatabaseManager();
         databaseManager.saveNewUser("lov456", newUser);
-    }
 
-    @Test
-    public void searchInfoLength() {
         ViewProfileInputManager viewProfileInputManager = new ViewProfileInputManager();
         ArrayList<String> info = viewProfileInputManager.searchInfo("lov456");
 
-        Assertions.assertEquals(11, info.size());
-    }
-
-    @Test
-    public void searchInfoCorrect() {
-        ViewProfileInputManager viewProfileInputManager = new ViewProfileInputManager();
-        ArrayList<String> info = viewProfileInputManager.searchInfo("lov456");
-
+        Assertions.assertEquals("lov456", info.get(0));
         Assertions.assertEquals("Lovina", info.get(1));
         Assertions.assertEquals("20", info.get(2));
         Assertions.assertEquals("She/Her", info.get(3));
